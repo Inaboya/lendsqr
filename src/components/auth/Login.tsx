f92f4622dcd7747ddf559a5de607594e146c5340
Inaboya <momohdavid21@gmail.com>
2022-10-16 15:12:58 +0100

    set up project

---
+++ src/components/auth/Login.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./login.css";
+
+export const Login: React.FC = () => {
+  return (
+    <div className="login">
+      <div className="login__container">
+        <div className="login-side-1">SIDE 1</div>
+        <div className="login-side-2">SIDE 2</div>
+      </div>
+    </div>
+  );
+};
